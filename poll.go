77b394d2ab54ef0413fd803bacd98d8ff5973020
Leibniz137 <nate@thenateway.io>
2020-06-13 17:23:09 -0700

    check if eth client is synced

--- poll.go
+++ poll.go
@@ -11,12 +11,14 @@ from template to be used by keep-client process, then halts with exit code 0.
 package main
 
 import (
+	"context"
 	"fmt"
+	"math/big"
 	"os"
 	"strings"
 	"time"
 
-  "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var RequiredEnvVars = [...]string{
@@ -32,20 +34,20 @@ func checkEnvVars(currentEnv []string) []string {
 
 	errors := make([]string, 0, len(RequiredEnvVars))
 
-  var found bool
-  var empty bool
-  var envValue []string
+	var found bool
+	var empty bool
+	var envValue []string
 	for _, requiredEnvVar := range RequiredEnvVars {
-    found = false
-    empty = false
+		found = false
+		empty = false
 		for _, envVar := range currentEnv {
 			if strings.HasPrefix(envVar, requiredEnvVar) {
 				found = true
-        envValue = strings.Split(envVar, "=")
-        if strings.TrimSpace(envValue[1]) == "" {
-          fmt.Println("empty value")
-          empty = true
-        }
+				envValue = strings.Split(envVar, "=")
+				if strings.TrimSpace(envValue[1]) == "" {
+					fmt.Println("empty value")
+					empty = true
+				}
 			}
 		}
 
@@ -59,26 +61,48 @@ func checkEnvVars(currentEnv []string) []string {
 func main() {
 	currentEnv := os.Environ()
 
-  var missingEnv []string
+	var missingEnv []string
 	for true {
-    missingEnv = checkEnvVars(currentEnv)
+		missingEnv = checkEnvVars(currentEnv)
 		if len(missingEnv) != 0 {
 			fmt.Printf("Missing required environment variables: %s, please set them and restart\n", missingEnv)
-      time.Sleep(10 * time.Second)
-      continue
+			time.Sleep(10 * time.Second)
+			continue
 		} else {
-      fmt.Println("Found all required environment variables")
-    }
+			fmt.Println("Found all required environment variables")
+		}
 
-    // try connecting to http rpc url
-    _, err := ethclient.Dial("https://mainnet.infura.io")
-    if err != nil {
-        fmt.Printf("Failed to connect to ethereum endpoint: %s\n", err)
-        time.Sleep(10 * time.Second)
-        continue
-    }
-    // idea try connecting to peers...
-    fmt.Println("Successfully connected to eth1 rpc endpoint")
-    break
+		var rpcUrl string
+		for _, envVar := range currentEnv {
+			if strings.HasPrefix(envVar, "HTTP_RPC_URL") {
+				rpcUrl = strings.Split(envVar, "=")[1]
+			}
+		}
+
+		// try connecting to http rpc url
+		client, err := ethclient.Dial(rpcUrl)
+		if err != nil {
+			fmt.Printf("Failed to connect to ethereum endpoint: %s\n", err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		fmt.Println("Successfully connected to eth1 rpc endpoint")
+
+    // check block number to see if eth client is synced
+		header, err := client.HeaderByNumber(context.Background(), nil)
+		if err != nil {
+			fmt.Printf("Failed to retrieve latest block header %s\n", err)
+      time.Sleep(10 * time.Second)
+			continue
+		}
+		zero := big.NewInt(int64(0))
+		blockNumber := header.Number
+		if blockNumber.Cmp(zero) == 1 {
+			fmt.Println("Eth endpoint is synchronized, starting keep client...")
+			break
+		}
+
+		time.Sleep(10 * time.Second)
 	}
 }
